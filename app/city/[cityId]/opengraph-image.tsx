652f5063ad83151bc298af0737f04d21fbcd5887
unknown <c.green5201@gmail.com>
2025-12-18 11:49:40 -0600

    Add dynamic open graph image for each city

---
+++ app/city/[cityId]/opengraph-image.tsx
@@ -0,0 +1,107 @@
+import { getAllCities } from "@/lib/city-data-source";
+import { ImageResponse } from "next/og";
+
+const width = 300;
+const height = 200;
+const padding = 40;
+
+export default async function GET({
+  params,
+}: {
+  params: Promise<{ cityId: string }>;
+}) {
+  const { cityId } = await params;
+  const cities = getAllCities();
+  const city = cities.find((c) => c.info.id == cityId) || cities[0];
+  const data = city.metrics.map((m) => ({
+    year: m.fiscalYear,
+    value: m.netFinancialPosition,
+  }));
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <div tw="text-black-600 mt-4" style={{ display: "flex" }}>
+          {city.info.name} - Net Financial Position
+        </div>
+        <LineGraph data={data} />
+      </div>
+    ),
+    { width, height }
+  );
+}
+
+type LineGraphProps = {
+  data: {
+    year: number;
+    value: number;
+  }[];
+};
+
+const LineGraph = ({ data }: LineGraphProps) => {
+  const years = data.map((d) => d.year);
+  const values = data.map((d) => d.value);
+
+  const minYear = Math.min(...years);
+  const maxYear = Math.max(...years);
+  const minValue = Math.min(...values);
+  const maxValue = Math.max(...values);
+
+  const xScale = (year: number): number =>
+    padding +
+    ((year - minYear) / (maxYear - minYear || 1)) * (width - padding * 2);
+
+  const yScale = (value: number): number =>
+    height -
+    padding -
+    ((value - minValue) / (maxValue - minValue || 1)) * (height - padding * 2);
+
+  const pathD: string = data
+    .map((d, i) => {
+      const x = xScale(d.year);
+      const y = yScale(d.value);
+      return `${i === 0 ? "M" : "L"} ${x} ${y}`;
+    })
+    .join(" ");
+
+  return (
+    <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
+      {/* X Axis */}
+      <line
+        x1={padding}
+        y1={height - padding}
+        x2={width - padding}
+        y2={height - padding}
+        stroke="grey"
+      />
+
+      {/* Y Axis */}
+      <line
+        x1={padding}
+        y1={padding}
+        x2={padding}
+        y2={height - padding}
+        stroke="grey"
+      />
+
+      {/* Rounded Line */}
+      <path
+        d={pathD}
+        fill="none"
+        stroke="#1449e69d"
+        strokeWidth={3}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
