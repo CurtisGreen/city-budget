1280aceb3e0e6581a6fbe9b5b00a9187cd702195
unknown <c.green5201@gmail.com>
2025-12-28 14:47:32 -0600

    Richardson and rowlett property tax rates

--- data/info/richardson.ts
+++ data/info/richardson.ts
@@ -13,6 +13,68 @@ export const richardsonInfo: CityInfo = {
     { year: 2025, value: 122745 },
   ],
   propertyTaxRate: 0.72,
+  propertyValues: [
+    {
+      fiscalYear: 2015,
+      moRate: 0.37031,
+      isRate: 0.26485,
+    },
+    {
+      fiscalYear: 2016,
+      moRate: 0.38031,
+      isRate: 0.25485,
+    },
+    {
+      fiscalYear: 2017,
+      moRate: 0.37031,
+      isRate: 0.25485,
+    },
+    {
+      fiscalYear: 2018,
+      moRate: 0.37031,
+      isRate: 0.25485,
+    },
+    {
+      fiscalYear: 2019,
+      moRate: 0.37413,
+      isRate: 0.25103,
+    },
+    {
+      fiscalYear: 2020,
+      moRate: 0.38124,
+      isRate: 0.24392,
+    },
+    {
+      fiscalYear: 2021,
+      moRate: 0.38124,
+      isRate: 0.24392,
+    },
+    {
+      fiscalYear: 2022,
+      moRate: 0.3772,
+      isRate: 0.23795,
+    },
+    {
+      fiscalYear: 2023,
+      moRate: 0.34316,
+      isRate: 0.21779,
+    },
+    {
+      fiscalYear: 2024,
+      moRate: 0.34316,
+      isRate: 0.21779,
+    },
+    {
+      fiscalYear: 2025,
+      moRate: 0.32439,
+      isRate: 0.21779,
+    },
+    {
+      fiscalYear: 2026,
+      moRate: 0.32439,
+      isRate: 0.21779,
+    },
+  ],
   salesTaxBreakdown: [
     { category: "Public Safety", percentage: 42, amount: 147000000 },
     { category: "Transportation", percentage: 24, amount: 84000000 },
--- data/info/rowlett.ts
+++ data/info/rowlett.ts
@@ -13,6 +13,68 @@ export const rowlettInfo: CityInfo = {
     { year: 2025, value: 67519 },
   ],
   propertyTaxRate: 0.72,
+  propertyValues: [
+    {
+      fiscalYear: 2015,
+      moRate: 0.54977,
+      isRate: 0.2374,
+    },
+    {
+      fiscalYear: 2016,
+      moRate: 0.57792,
+      isRate: 0.20925,
+    },
+    {
+      fiscalYear: 2017,
+      moRate: 0.55117,
+      isRate: 0.236,
+    },
+    {
+      fiscalYear: 2018,
+      moRate: 0.55119,
+      isRate: 0.22598,
+    },
+    {
+      fiscalYear: 2019,
+      moRate: 0.53113,
+      isRate: 0.22604,
+    },
+    {
+      fiscalYear: 2020,
+      moRate: 0.53756,
+      isRate: 0.21961,
+    },
+    {
+      fiscalYear: 2021,
+      moRate: 0.52469,
+      isRate: 0.22031,
+    },
+    {
+      fiscalYear: 2022,
+      moRate: 0.53854,
+      isRate: 0.20646,
+    },
+    {
+      fiscalYear: 2023,
+      moRate: 0.52579,
+      isRate: 0.15522,
+    },
+    {
+      fiscalYear: 2024,
+      moRate: 0.53134,
+      isRate: 0.17906,
+    },
+    {
+      fiscalYear: 2025,
+      moRate: 0.531341,
+      isRate: 0.23835,
+    },
+    {
+      fiscalYear: 2026,
+      moRate: 0.538841,
+      isRate: 0.26905,
+    },
+  ],
   salesTaxBreakdown: [
     { category: "Public Safety", percentage: 42, amount: 147000000 },
     { category: "Transportation", percentage: 24, amount: 84000000 },
