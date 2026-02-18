4b1893138cdddef03c53f9ff2f4068d91e176048
unknown <c.green5201@gmail.com>
2026-02-17 23:33:24 -0600

    Add NRH basic info

---
+++ data/info/north-richland-hills.ts
@@ -0,0 +1,83 @@
+import { CityInfo } from "@/lib/types";
+
+export const northRichlandHillsInfo: CityInfo = {
+  id: "north-richland-hills",
+  name: "North Richland Hills",
+  population: 74859,
+  populations: [
+    { year: 1980, value: 30592 },
+    { year: 1990, value: 45895 },
+    { year: 2000, value: 55635 },
+    { year: 2010, value: 63343 },
+    { year: 2020, value: 69917 },
+    { year: 2025, value: 74859 },
+  ],
+  propertyValues: [
+    {
+      fiscalYear: 2015,
+      moRate: 0.35058,
+      isRate: 0.25942,
+    },
+    {
+      fiscalYear: 2016,
+      moRate: 0.36473,
+      isRate: 0.24527,
+    },
+    {
+      fiscalYear: 2017,
+      moRate: 0.348083,
+      isRate: 0.261917,
+    },
+    {
+      fiscalYear: 2018,
+      moRate: 0.32864,
+      isRate: 0.26136,
+    },
+    {
+      fiscalYear: 2019,
+      moRate: 0.327088,
+      isRate: 0.257912,
+    },
+    {
+      fiscalYear: 2020,
+      moRate: 0.33494,
+      isRate: 0.23706,
+    },
+    {
+      fiscalYear: 2021,
+      moRate: 0.343611,
+      isRate: 0.232089,
+    },
+    {
+      fiscalYear: 2022,
+      moRate: 0.348864,
+      isRate: 0.22332,
+    },
+    {
+      fiscalYear: 2023,
+      moRate: 0.347742,
+      isRate: 0.20023,
+    },
+    {
+      fiscalYear: 2024,
+      moRate: 0.313579,
+      isRate: 0.17581,
+    },
+    {
+      fiscalYear: 2025,
+      moRate: 0.331432,
+      isRate: 0.157957,
+    },
+    {
+      fiscalYear: 2026,
+      moRate: 0.346946,
+      isRate: 0.150895,
+    },
+  ],
+  revenueBySource: {
+    property: 39174000,
+    sales: 27974000,
+    hotel: 446000,
+  },
+  area: 18.43,
+};
